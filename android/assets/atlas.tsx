112b806418b33674ff4dc6fe43a5bd484f95df8e
Dunkansdk <esqueje18@gmail.com>
2018-06-30 19:54:43 -0300

    bodies

--- android/assets/atlas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="atlas" tilewidth="32" tileheight="32" tilecount="2048" columns="64">
- <image source="atlas.png" width="2048" height="1024"/>
-</tileset>
