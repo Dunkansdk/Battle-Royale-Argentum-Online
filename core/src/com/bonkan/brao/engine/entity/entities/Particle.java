bb09bf3396e1b11a4070b38bcf1d958768d7f730
Manuel Liste <listemanuel95@gmail.com>
2018-07-07 01:01:17 -0300

    alto mapa

--- core/src/com/bonkan/brao/engine/entity/entities/Particle.java
+++ core/src/com/bonkan/brao/engine/entity/entities/Particle.java
@@ -1,5 +1,32 @@
 package com.bonkan.brao.engine.entity.entities;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.bonkan.brao.engine.entity.Entity;
+
 public class Particle extends Entity {
+
+	public Particle(TextureRegion texture, int x, int y) {
+		super(texture, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
